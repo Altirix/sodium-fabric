464afb394afbdc518d3f50caf8e0f03e1b2487f9
Altirix <19439141+Altirix@users.noreply.github.com>
2022-12-15 21:15:06 +0000

    better to just check if player is inside the cloud and render the faces that are otherwise hidden
    culling occurs when the player is behind the face. rather than only working on the negative x/z axis and 16 clouds away on the positive x/z axis (128 blocks?)

--- src/main/java/net/caffeinemc/sodium/mixin/features/sky/MixinWorldRenderer.java
+++ src/main/java/net/caffeinemc/sodium/mixin/features/sky/MixinWorldRenderer.java
@@ -1,19 +1,20 @@
 package net.caffeinemc.sodium.mixin.features.sky;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.caffeinemc.sodium.SodiumClientMod;
+import net.caffeinemc.sodium.config.user.UserConfig;
 import net.caffeinemc.sodium.render.terrain.CloudRenderer;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.CloudRenderMode;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
-import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static net.caffeinemc.sodium.render.terrain.CloudRenderer.TEXTURE_OFFSET;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
@@ -56,26 +57,21 @@ public class MixinWorldRenderer {
      *  - at extreme angles inside the cloud it may be possible for a false positive to cull in the Negative X/Y/Z from the player (North, West, Down)
      *  - <a href="https://bugs.mojang.com/browse/MC-205851">MC-205851</a>
      *  - will cull cloud faces Positive X/Z (South, East) 16 cloud cubes (2 cloud chunks) away from the player.
+     *  - regardless of render distance the cloud map will always be 64 x 64 for both fast (stride 32) and fancy (stride 8)
+     *  - a cloud chunk is 64 blocks wide(?)
      * Possible Fixes
      *  - Option 1:
      *      - It is possible to reduce this to 8 cloud cubes (1 cloud chunk) away from the player easily with no visual impact
      *  - Option 2:
      *      - It is possible to reduce this to 1 cloud cube away from the player, but this is more involved as
      *      -  The South/East faces will be culled incorrectly when the player is inside the cloud
-     *      -- Method 1:
-     *          -- It is possible to solve this bug by offsetting the starting position of positive cloud faces by 1
-     *          -- to make textures render in the correct position the offset is taken away from the U/V
-     *          -- However this causes the last faces to not render at the edge of the cloud map as we iterate 1 less in Negative X/Z
-     *          -- it is possible to fix by just manually including 1 face extra South/East face in the Negative X/Z directions.
-     *      -- Method 2:
-     *          -- it should be possible to check if the player is inside a cloud using the CloudY position
-     *          -- then enable the specific clouds South/East Face
-     *          -- should be less hacky
+     *      - Check If Player Is In Cloud
+     *          -- enable that clouds South/East Face
      */
     @Overwrite
     private BufferBuilder.BuiltBuffer renderClouds(BufferBuilder builder, double x, double y, double z, Vec3d color) {
-        float k = (float) MathHelper.floor(x) * 0.00390625F;
-        float l = (float) MathHelper.floor(z) * 0.00390625F;
+        float k = (float) MathHelper.floor(x) * TEXTURE_OFFSET;
+        float l = (float) MathHelper.floor(z) * TEXTURE_OFFSET;
 
         float red = (float)color.x;
         float green = (float)color.y;
@@ -85,42 +81,38 @@ public class MixinWorldRenderer {
         RenderSystem.setShader(GameRenderer::getPositionTexColorNormalShader);
         builder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR_NORMAL);
         float cloudY = (float)Math.floor(y / 4.0) * 4.0F;
-        float cloudX = 0;
-        float cloudZ = 0;
 
         if (this.lastCloudRenderMode == CloudRenderMode.FANCY) {
+
+            if (cloudY < 1F && cloudY > -(CloudRenderer.CLOUD_HEIGHT + 1)) { // Inside Cloud;
+                CloudRenderer.renderCloudInsideEastFace(builder,0,cloudY,0,k,l,red,green,blue);
+                CloudRenderer.renderCloudInsideSouthFace(builder,0,cloudY,0,k,l,red,green,blue);
+            }
             for(int cloudChunkX = -4; cloudChunkX < 4; ++cloudChunkX) {
                 for(int cloudChunkZ = -4; cloudChunkZ < 4; ++cloudChunkZ) {
 
-                    cloudX = (float)(cloudChunkX * 8);
-                    cloudZ = (float)(cloudChunkZ * 8);
+                    float cloudX = (float)(cloudChunkX * 8);
+                    float cloudZ = (float)(cloudChunkZ * 8);
 
-                    if (cloudY > -5.5F) {  // Bottom Face
+                    if (cloudY > -(CloudRenderer.CLOUD_HEIGHT + 1)) {  // Bottom Face
                         CloudRenderer.renderCloudBottomFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
-
-                    if (cloudY <= 5.0F) { // Top Face
+                    if (cloudY < 1F) { // Top Face
                         CloudRenderer.renderCloudTopFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
-
                     if (cloudChunkX > -1) { // West Face
                         CloudRenderer.renderCloudWestFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
-
-                    if (cloudChunkX < 0) { // East Face
+                    else { // East Face
                         CloudRenderer.renderCloudEastFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
-
                     if (cloudChunkZ > -1) { // North Face
                         CloudRenderer.renderCloudNorthFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
-
-                    if (cloudChunkZ < 0) { // South Face
+                    else { // South Face
                         CloudRenderer.renderCloudSouthFace(builder,cloudX,cloudY,cloudZ,k,l,red,green,blue);
                     }
                 }
-                CloudRenderer.renderCloudSingleEastFace(builder,-4 * 8 - 1,cloudY,cloudX,k,l,red,green,blue);// just need to iterate a single axis
-                CloudRenderer.renderCloudSingleSouthFace(builder,cloudX,cloudY,-4 * 8 - 1,k,l,red,green,blue);
             }
         } else {
             CloudRenderer.renderCloudsFast(builder,cloudY,k,l,red,green,blue);
--- src/main/java/net/caffeinemc/sodium/render/terrain/CloudRenderer.java
+++ src/main/java/net/caffeinemc/sodium/render/terrain/CloudRenderer.java
@@ -5,10 +5,9 @@ import net.minecraft.client.render.BufferBuilder;
 public class CloudRenderer {
     private static final float EPSILON = 0.001F;
     private static final float CLOUD_WIDTH = 8.0F;
-    private static final float CLOUD_HEIGHT = 4.0F;
-    private static final float CLOUD_START_OFFSET = 2.0F;
-    private static final float CLOUD_START_OFFSET_TEXTURE = -1.0F;
-    private static final float TEXTURE_OFFSET = 0.00390625F;
+    public static final float CLOUD_HEIGHT = 4.0F;
+    private static final float CLOUD_START_OFFSET = 1.0F;
+    public static final float TEXTURE_OFFSET = 0.00390625F;
     private static final float TEXTURE_OTHER_OFFSET = 0.5F;
     public static void renderCloudBottomFace(BufferBuilder builder, float cloudX, float cloudY, float cloudZ, float k, float l, float r, float g, float b) {
         float red = r * 0.7F;
@@ -102,50 +101,50 @@ public class CloudRenderer {
         for(int cloudPositionInChunk = 0; cloudPositionInChunk < 8; ++cloudPositionInChunk) {
 
             builder.vertex(cloudX + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON, cloudY, cloudZ + CLOUD_WIDTH)
-                    .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
+                    .texture((cloudX + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(1.0F, 0.0F, 0.0F)
                     .next();
             builder.vertex(cloudX + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON, cloudY + CLOUD_HEIGHT, cloudZ + CLOUD_WIDTH)
-                    .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
+                    .texture((cloudX + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(1.0F, 0.0F, 0.0F)
                     .next();
             builder.vertex(cloudX + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON, cloudY + CLOUD_HEIGHT, cloudZ)
-                    .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
+                    .texture((cloudX + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(1.0F, 0.0F, 0.0F)
                     .next();
             builder.vertex(cloudX + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON, cloudY, cloudZ)
-                    .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
+                    .texture((cloudX + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(1.0F, 0.0F, 0.0F)
                     .next();
 
         }
     }
-    public static void renderCloudSingleEastFace(BufferBuilder builder, float cloudX, float cloudY, float cloudZ, float k, float l, float r, float g, float b) {
+    public static void renderCloudInsideEastFace(BufferBuilder builder, float cloudX, float cloudY, float cloudZ, float k, float l, float r, float g, float b) {
         float red = r * 0.9F;
         float green = g * 0.9F;
         float blue = b * 0.9F;
 
         builder.vertex(cloudX + CLOUD_START_OFFSET - EPSILON, cloudY, cloudZ + CLOUD_WIDTH)
-                .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
+                .texture((cloudX  + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(1.0F, 0.0F, 0.0F)
                 .next();
         builder.vertex(cloudX + CLOUD_START_OFFSET - EPSILON, cloudY + CLOUD_HEIGHT, cloudZ + CLOUD_WIDTH)
-                .texture((cloudX - CLOUD_START_OFFSET_TEXTURE  + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
+                .texture((cloudX + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, (cloudZ + CLOUD_WIDTH) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(1.0F, 0.0F, 0.0F)
                 .next();
         builder.vertex(cloudX + CLOUD_START_OFFSET - EPSILON, cloudY + CLOUD_HEIGHT, cloudZ)
-                .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
+                .texture((cloudX + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(1.0F, 0.0F, 0.0F)
                 .next();
         builder.vertex(cloudX  + CLOUD_START_OFFSET - EPSILON, cloudY, cloudZ)
-                .texture((cloudX - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
+                .texture((cloudX + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + k, cloudZ * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(1.0F, 0.0F, 0.0F)
                 .next();
@@ -190,50 +189,50 @@ public class CloudRenderer {
         for(int cloudPositionInChunk = 0; cloudPositionInChunk < 8; ++cloudPositionInChunk) {
 
             builder.vertex(cloudX, cloudY + CLOUD_HEIGHT, cloudZ + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON)
-                    .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                    .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(0.0F, 0.0F, 1.0F)
                     .next();
             builder.vertex(cloudX + CLOUD_WIDTH, cloudY + CLOUD_HEIGHT, cloudZ + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON)
-                    .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                    .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(0.0F, 0.0F, 1.0F)
                     .next();
             builder.vertex(cloudX + CLOUD_WIDTH, cloudY, cloudZ + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON)
-                    .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                    .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(0.0F, 0.0F, 1.0F)
                     .next();
             builder.vertex(cloudX, cloudY, cloudZ + cloudPositionInChunk + CLOUD_START_OFFSET - EPSILON)
-                    .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                    .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ + cloudPositionInChunk + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                     .color(red, green, blue, 0.8F)
                     .normal(0.0F, 0.0F, 1.0F)
                     .next();
         }
     }
 
-    public static void renderCloudSingleSouthFace(BufferBuilder builder, float cloudX, float cloudY, float cloudZ, float k, float l, float r, float g, float b) {
+    public static void renderCloudInsideSouthFace(BufferBuilder builder, float cloudX, float cloudY, float cloudZ, float k, float l, float r, float g, float b) {
         float red = r * 0.8F;
         float green = g * 0.8F;
         float blue = b * 0.8F;
 
         builder.vertex(cloudX, cloudY + CLOUD_HEIGHT, cloudZ + CLOUD_START_OFFSET - EPSILON)
-                .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(0.0F, 0.0F, 1.0F)
                 .next();
         builder.vertex(cloudX + CLOUD_WIDTH, cloudY + CLOUD_HEIGHT, cloudZ + CLOUD_START_OFFSET - EPSILON)
-                .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(0.0F, 0.0F, 1.0F)
                 .next();
         builder.vertex(cloudX + CLOUD_WIDTH, cloudY, cloudZ + CLOUD_START_OFFSET - EPSILON)
-                .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                .texture((cloudX + CLOUD_WIDTH) * TEXTURE_OFFSET + k, (cloudZ + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(0.0F, 0.0F, 1.0F)
                 .next();
         builder.vertex(cloudX, cloudY, cloudZ  + CLOUD_START_OFFSET - EPSILON)
-                .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ - CLOUD_START_OFFSET_TEXTURE + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
+                .texture(cloudX * TEXTURE_OFFSET + k, (cloudZ + TEXTURE_OTHER_OFFSET) * TEXTURE_OFFSET + l)
                 .color(red, green, blue, 0.8F)
                 .normal(0.0F, 0.0F, 1.0F)
                 .next();

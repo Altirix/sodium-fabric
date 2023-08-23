ef6898edd12eb45910e5684a2ac1f66649f20f49
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-22 22:10:05 -0300

    Update PostLaunchChecks.java

--- src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/PostLaunchChecks.java
+++ src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/PostLaunchChecks.java
@@ -16,13 +16,6 @@ public class PostLaunchChecks {
 
     public static void checkContext() {
         checkContextImplementation();
-
-        if (isUsingPojavLauncher()) {
-            showConsoleMessage(Text.translatable("sodium.console.pojav_launcher"));
-            logMessage("It appears that PojavLauncher is being used with an OpenGL compatibility layer. This will " +
-                    "likely cause severe performance issues, graphical issues, and crashes when used with Sodium. This " +
-                    "configuration is not supported -- you are on your own!");
-        }
     }
 
     private static void checkContextImplementation() {
@@ -38,7 +31,7 @@ public class PostLaunchChecks {
         LOGGER.info("OpenGL Version: {}", driver.version());
 
         if (isBrokenNvidiaDriverInstalled(driver)) {
-            showConsoleMessage(Text.translatable("sodium.console.broken_nvidia_driver"));
+            showConsoleMessage(Text.of("sodium.console.broken_nvidia_driver"));
             logMessage("The NVIDIA graphics driver appears to be out of date. This will likely cause severe " +
                     "performance issues and crashes when used with Sodium. The graphics driver should be updated to " +
                     "the latest version (version 536.23 or newer).");
@@ -66,13 +59,7 @@ public class PostLaunchChecks {
         LOGGER.error(message, args);
     }
 
-    // https://github.com/CaffeineMC/sodium-fabric/issues/1486
-    // The way which NVIDIA tries to detect the Minecraft process could not be circumvented until fairly recently
-    // So we require that an up-to-date graphics driver is installed so that our workarounds can disable the Threaded
-    // Optimizations driver hack.
     private static boolean isBrokenNvidiaDriverInstalled(GLContextInfo driver) {
-        // The Linux driver has two separate branches which have overlapping version numbers, despite also having
-        // different feature sets. As a result, we can't reliably determine which Linux drivers are broken...
         if (Util.getOperatingSystem() != Util.OperatingSystem.WINDOWS) {
             return false;
         }
@@ -88,40 +75,4 @@ public class PostLaunchChecks {
 
         return false;
     }
-
-    // https://github.com/CaffeineMC/sodium-fabric/issues/1916
-    private static boolean isUsingPojavLauncher() {
-        if (System.getenv("POJAV_RENDERER") != null) {
-            LOGGER.warn("Detected presence of environment variable POJAV_LAUNCHER, which seems to indicate we are running on Android");
-
-            return true;
-        }
-
-        var librarySearchPaths = System.getProperty("java.library.path", null);
-
-        if (librarySearchPaths != null) {
-            for (var path : librarySearchPaths.split(":")) {
-                if (isKnownAndroidPathFragment(path)) {
-                    LOGGER.warn("Found a library search path which seems to be hosted in an Android filesystem: {}", path);
-
-                    return true;
-                }
-            }
-        }
-
-        var workingDirectory = System.getProperty("user.home", null);
-
-        if (workingDirectory != null) {
-            if (isKnownAndroidPathFragment(workingDirectory)) {
-                LOGGER.warn("Working directory seems to be hosted in an Android filesystem: {}", workingDirectory);
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean isKnownAndroidPathFragment(String path) {
-        return path.matches("/data/user/[0-9]+/net\\.kdt\\.pojavlaunch");
-    }
-
 }

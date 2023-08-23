aa81ceb41b84481ae1478bc1fefc7116572d9842
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-22 22:03:34 -0300

    Update WorldRendererExtended.java

--- src/main/java/me/jellysquid/mods/sodium/client/world/WorldRendererExtended.java
+++ src/main/java/me/jellysquid/mods/sodium/client/world/WorldRendererExtended.java
@@ -2,9 +2,6 @@ package me.jellysquid.mods.sodium.client.world;
 
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 
-public abstract class WorldRendererMixin extends WorldRenderer implements WorldRendererExtended {
-    @Override
-    public WorldRenderer getWorldRenderer() {
-        return this;
-    }
+public interface WorldRendererExtended {
+    SodiumWorldRenderer sodium$getWorldRenderer();
 }
